0545ada3babeee58e3f9984bef81b4ea27bb64ae
Yesid Marin <yesid@autentikar.com>
2024-03-25 17:52:41 -0500

    version 4.3.0-beta

--- AKCoreSwiftWrapper/dummy.m
+++
@@ -1,7 +0,0 @@
-//
-//  dummy.m
-//  
-//
-//  Created by Yesid Marin on 21/06/23.
-//
-// Swift Package Manager needs at least one source file.
--- AKCoreSwiftWrapper/include/dummy.h
+++
@@ -1,7 +0,0 @@
-//
-//  Header.h
-//  
-//
-//  Created by Yesid Marin on 21/06/23.
-//
-// Swift Package Manager needs at least one header to prevent a warning.
